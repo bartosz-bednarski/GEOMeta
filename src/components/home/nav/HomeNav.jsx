36454f1756e67b85582addf888924c4642f5876a
Bartosz Bednarski <bartosz.bednarski97@gmail.com>
2023-07-20 21:55:28 +0200

    Created redirect to pages thru navMenu

--- src/components/home/nav/HomeNav.jsx
+++ src/components/home/nav/HomeNav.jsx
@@ -1,14 +1,16 @@
+import { Router, useNavigate } from "react-router-dom";
 import classes from "./HomeNav.module.scss";
 const HomeNav = () => {
+  const navigate = useNavigate();
   return (
     <nav className={classes.navigation}>
       <ul>
-        <li>Home</li>
-        <li>Kontynenty</li>
-        <li>Wyspy</li>
-        <li>Trekkery</li>
-        <li>Inne</li>
-        <li>Forum</li>
+        <li onClick={() => navigate("/")}>Home</li>
+        <li onClick={() => navigate("/continents")}>Kontynenty</li>
+        <li onClick={() => navigate("/islands")}>Wyspy</li>
+        <li onClick={() => navigate("/trekkers")}>Trekkery</li>
+        <li onClick={() => navigate("/others")}>Inne</li>
+        <li onClick={() => navigate("/forum")}>Forum</li>
       </ul>
     </nav>
   );
