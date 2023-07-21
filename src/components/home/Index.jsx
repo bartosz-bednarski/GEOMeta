0b64be2355df448fad5bd4a4269fc0e0a2ac15ad
Bartosz Bednarski <bartosz.bednarski97@gmail.com>
2023-07-21 20:41:42 +0200

    Created elements for homePage

--- src/components/home/Index.jsx
+++ src/components/home/Index.jsx
@@ -1,13 +1,38 @@
 import HomeNav from "./nav/HomeNav";
 import classes from "./home.module.scss";
 import Logo from "../tools/Logo";
+import bookImg from "../../assets/images/home/open-book.1024x822.png";
+import worldImg from "../../assets/images/home/world-connection.svg";
 const Home = () => {
   return (
     <div className={classes.home}>
-      <div className={classes["nav-container"]}>
+      <div className={classes["home__nav-container"]}>
         <Logo />
         <HomeNav />
+        <span />
       </div>
+      <main className={classes["home__main-container"]}>
+        <div className={classes["home__main-container__images-box"]}>
+          <img
+            src={bookImg}
+            className={classes["home__main-container__images-box__book"]}
+          />
+          <img
+            src={worldImg}
+            className={classes["home__main-container__images-box__world"]}
+          />
+        </div>
+        <div className={classes["home__main-container__text-box"]}>
+          <span className={classes["home__main-container__text-box__logo"]}>
+            GEOMeta
+          </span>
+          <span className={classes["home__main-container__text-box__text"]}>
+            Twój kompleksowy przewodnik po grze GeoGuessr, który dostarczy Ci
+            niezbędnych narzędzi i wskazówek, abyś mógł odkrywać świat w pełni
+            świadomie i skutecznie!
+          </span>
+        </div>
+      </main>
     </div>
   );
 };
