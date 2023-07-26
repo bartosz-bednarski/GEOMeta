00db2d77bfa63f8b07432084bf861202b5877d6b
Bartosz <104717320+bartosz-bednarski@users.noreply.github.com>
2023-07-26 22:02:31 +0200

    check (#15)
    * Dev (#8)
    
    * Added section at homePage
    
    * Fixed nav path
    
    * Dev (#9)
    
    * Added section at homePage
    
    * Fixed nav path
    
    * Fixed build bug
    
    * Dev (#11)
    
    * Added section at homePage
    
    * Fixed nav path
    
    * Fixed build bug
    
    * Dev (#9) (#10)
    
    * Added section at homePage
    
    * Fixed nav path
    
    * Fixed build bug
    
    * Dev (#12)
    
    * Added section at homePage
    
    * Fixed nav path
    
    * Fixed build bug
    
    * Dev (#9) (#10)
    
    * Added section at homePage
    
    * Fixed nav path
    
    * Fixed build bug
    
    * Dev (#13)
    
    * Added section at homePage
    
    * Fixed nav path
    
    * Fixed build bug
    
    * Dev (#9) (#10)
    
    * Added section at homePage
    
    * Fixed nav path
    
    * Fixed build bug
    
    * Dev (#14)
    
    * Added section at homePage
    
    * Fixed nav path
    
    * Fixed build bug
    
    * Dev (#9) (#10)
    
    * Added section at homePage
    
    * Fixed nav path
    
    * Fixed build bug
    
    * Merge
    
    * Fix

--- src/components/home/Index.jsx
+++ src/components/home/Index.jsx
@@ -1,11 +1,9 @@
 import classes from "./home.module.scss";
-import africaImg from "../../assets/images/home/africa.1024x977.png";
+import LogoSmall from "../tools/LogoSmall";
+import Africa from "../../assets/images/home/africa.1024x977.png";
 import LogoBig from "../tools/LogoBig";
 import worldBookImg from "../../assets/images/home/grafika1v21.png";
-import lemurImg from "../../assets/images/home/lemur.png";
-import rucksackImg from "../../assets/images/home/rucksack.png";
-import statueOfLibertyImg from "../../assets/images/home/statueofliberty.png";
-import forumImg from "../../assets/images/home/forum.png";
+import lemur from "../../assets/images/home/lemur.png";
 const Home = () => {
   return (
     <div className={classes.home}>
@@ -27,10 +25,7 @@ const Home = () => {
         className={`${classes["home__section"]} ${classes["home__section__left"]}`}
       >
         <div className={classes["home__section__content"]}>
-          <img
-            src={africaImg}
-            className={classes["home__section__content-img"]}
-          />
+          <img src={Africa} className={classes["home__section__content-img"]} />
           <span
             className={`${classes["home__section__content-text"]} ${classes["home-border-right"]}`}
           >
@@ -79,100 +74,31 @@ const Home = () => {
               naszej planecie.
             </p>
           </span>
-          <img
-            src={lemurImg}
-            className={classes["home__section__content-img"]}
-          />
+          <img src={lemur} className={classes["home__section__content-img"]} />
         </div>
       </section>
       <section
         className={`${classes["home__section"]} ${classes["home__section__left"]}`}
       >
         <div className={classes["home__section__content"]}>
-          <img
-            src={rucksackImg}
-            className={classes["home__section__content-img"]}
-          />
+          <img src={Africa} className={classes["home__section__content-img"]} />
           <span
             className={`${classes["home__section__content-text"]} ${classes["home-corners-left"]}`}
           >
-            <h1>Trekkery</h1>
+            <h1>Kontynenty</h1>
             <br />
             <p>
-              Pokażemy Ci charakterystyczne "trekkery" - zaawansowane urządzenia
-              Google, które pozwoliły uwiecznić nieosiągalne dla samochodów
-              zakątki świata.
+              Doskonały sposób na naukę fascynujących ciekawostek o kontynentach
+              i ich unikalnych cechach.
             </p>
             <br />
             <p>
-              Dzięki nim będziesz mógł odkryć malownicze krajobrazy i ukryte
-              perły, które zostały sfotografowane podczas wędrówek pieszych,
-              wypraw górskich czy nawet morskich ekspedycji. Przygotuj się na
-              niezapomniane wirtualne podróże, podczas których zaprezentujemy
-              unikalne miejsca, dostępne dzięki niezwykłym trekkerom Google!
-            </p>
-          </span>
-        </div>
-        <div className={classes["home__section__no-content"]} />
-      </section>
-      <section
-        className={`${classes["home__section"]} ${classes["home__section__right"]}`}
-      >
-        <div className={classes["home__section__no-content"]}>
-          <span />
-          <span
-            className={`${classes["home-border-bottom"]} ${classes["home-border-top"]}`}
-          />
-        </div>
-        <div className={classes["home__section__content"]}>
-          <span
-            className={`${classes["home__section__content-text"]} ${classes["home-border-top"]} ${classes["home-border-bottom"]} ${classes["home-corners-right"]}`}
-          >
-            <h1>Inne</h1>
-            <br />
-            <p>
-              Istnieje wiele innych unikalnych elementów, które pozwalają
-              rozpoznać różne miejsca na świecie.
-            </p>
-            <br />
-            <p>
-              W naszym przewodniku przedstawimy Ci te wyjątkowe cechy, takie jak
-              ikoniczne budowle, charakterystyczne krajobrazy, kulturowe
-              elementy oraz lokalne zwyczaje, które wyróżniają poszczególne
-              regiony. Dzięki temu będziesz mógł zgłębiać tajemnice świata,
-              odkrywając jego różnorodność i bogactwo bez wychodzenia z domu!
-            </p>
-          </span>
-          <img
-            src={statueOfLibertyImg}
-            className={classes["home__section__content-img"]}
-          />
-        </div>
-      </section>
-      <section
-        className={`${classes["home__section"]} ${classes["home__section__left"]}`}
-      >
-        <div className={classes["home__section__content"]}>
-          <img
-            src={forumImg}
-            className={classes["home__section__content-img"]}
-          />
-          <span
-            className={`${classes["home__section__content-text"]} ${classes["home-corners-left"]}`}
-          >
-            <h1>Forum</h1>
-            <br />
-            <p>
-              Nasze forum to miejsce, gdzie pasjonaci i eksperci mogą ze sobą
-              dzielić swoją wiedzą, doświadczeniem i pasją.
-            </p>
-            <br />
-            <p>
-              To platforma, która stwarza możliwość nawiązywania wartościowych
-              interakcji, gdzie użytkownicy wspierają się nawzajem, odpowiadając
-              na pytania i udzielając cennych wskazówek. Wspólnie tworzymy
-              dynamiczną społeczność, której celem jest wzajemna pomoc i wzrost
-              wiedzy we GeoGuessrowych podrózach.
+              Podczas podróży wirtualnymi ścieżkami po całym globie, będziesz
+              mógł odkryć różnorodność kulturową, krajobrazy, zwierzęta i
+              rośliny, co pozwoli Ci na zgłębianie swojej wiedzy geograficznej w
+              niezwykle angażujący sposób. Poznawanie tych informacji będzie nie
+              tylko edukacyjne, ale także inspirujące, zachęcając Cię do
+              odkrywania różnorodności naszej planety!
             </p>
           </span>
         </div>
